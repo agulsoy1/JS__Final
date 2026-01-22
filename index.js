f22508bf0343ee10152e500a49d86a3a83acade9
agulsoy1 <alexandretg99@gmail.com>
2026-01-22 01:01:19 -0800

    Final Touches

---
+++ index.js
@@ -0,0 +1,35 @@
+const homeSearchBtn = document.querySelector(".header__search--icon");
+const homeSearchBar = document.querySelector(".header__search--bar");
+
+if(homeSearchBtn){   
+    homeSearchBtn.addEventListener('click', function(event){
+        if(event.type === "click"){
+            event.preventDefault();
+            showMovieBrowser();
+        }
+    });
+}
+
+if(homeSearchBar){
+    homeSearchBar.addEventListener('keydown', function(event){
+        if(event.key === "Enter"){
+            event.preventDefault();
+            showMovieBrowser();
+        }
+    });
+}
+    
+function showMovieBrowser(){
+    const homeSearchTerm = homeSearchBar ? homeSearchBar.value: "";
+    localStorage.setItem("query", homeSearchTerm);
+    // window.location.href = `${window.location.origin}/movie.html#`;
+    window.location.href = "movie.html";
+}
+
+function openMenu(){
+    document.body.classList += " menu--open";
+}
+
+function closeMenu(){
+    document.body.classList.remove("menu--open");
+}
---
+++ movie.js
@@ -0,0 +1,171 @@
+const searchBar = document.querySelector(".browse__searchbar");
+// const oldMovieDisplayELem = document.querySelector(".movie__display");
+const releaseDate = document.querySelector(".movie__year");
+const loading = document.querySelector(".movie__loading--state");
+const searchBtn = document.querySelector(".browse__search--icon");
+const optionSelect = document.querySelector("#years");
+const query = localStorage.getItem("query");
+
+// oldMovieDisplayELem.classList += " movie__display--hidden";
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM fully loaded and parsed");
+  loading.classList += " loading__state";
+  if (query !== null) {
+    searchBar.value = query;
+    localStorage.removeItem("query");
+    fetchData();
+  }
+});
+
+searchBtn.addEventListener("click", function (event) {
+  if (event.type === "click") {
+    event.preventDefault();
+    fetchData();
+  }
+});
+
+searchBar.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    fetchData();
+  }
+});
+
+function showLoading() {
+  loading.classList += " loading__state";
+}
+
+async function searchMovies() {
+  const searchTerm = searchBar.value;
+  const response = await fetch(
+    `https://www.omdbapi.com/?apikey=6d4005a9&s=${searchTerm}`
+  );
+  const data = await response.json();
+  const movieDisplayELem = document.querySelector(".movie__container");
+
+  // Create a card for each movie
+  data.Search.forEach((movie) => {
+    const movieCard = document.createElement("div");
+    movieCard.classList.add("movie__display", "movie-card");
+    movieCard.setAttribute("data-year", movie.Year); // store year for filtering
+
+    movieCard.innerHTML = `
+                <figure class="movie__poster--wrapper">
+                    <img src="${movie.Poster}" class="movie__poster" alt="">
+                </figure>
+                <div class="movie__info">
+                    <h3 class="movie__title">${movie.Title}</h3>
+                    <div id="movie__year">${movie.Year}</div>
+                </div>
+            `;
+    console.log(movieCard);
+    movieDisplayELem.appendChild(movieCard);
+  });
+}
+
+function clearMovies() {
+  const movieCards = document.querySelectorAll(".movie-card");
+  movieCards.forEach((card) => {
+    card.remove();
+  });
+}
+
+function fetchData() {
+  clearMovies();
+  showLoading();
+  setTimeout(() => {
+    // oldMovieDisplayELem.classList.remove("movie__display--hidden");
+    loading.classList += " loading__state--hidden";
+    searchMovies();
+  });
+}
+
+function showHomePage() {
+  window.location.href = "index.html";
+}
+
+optionSelect.addEventListener("change", function (event) {
+  const selectedOption = event.target.value;
+  const movieCards = document.querySelectorAll(".movie__display");
+  movieCards.forEach((card) => { 
+    const year = parseInt(card.getAttribute("data-year"));
+    // console.log(year);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+    // console.log(selectedOption);
+    switch (selectedOption) {
+      case "None":
+        card.style.display = "block";
+        break;
+      case "1930s":
+        if (1930 <= year && year <= 1939) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "1940s":
+        if (1940 <= year && year <= 1949) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "1950s":
+        if (1950 <= year && year <= 1959) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "1960s":
+        if (1960 <= year && year <= 1969) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "1970s":
+        if (1970 <= year && year <= 1979) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "1980s":
+        if (1980 <= year && year <= 1989) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "1990s":
+        if (1990 <= year && year <= 1999) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "2000s":
+        if (2000 <= year && year <= 2009) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "2010s":
+        if (2010 <= year && year <= 2019) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+      case "2020s":
+        if (2020 <= year && year <= 2024) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+        break;
+    }
+  });
+});
